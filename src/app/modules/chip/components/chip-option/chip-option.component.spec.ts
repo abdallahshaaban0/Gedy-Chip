96a61210537fe519f58dbc0df7676b44f6e8bc03
AbdullahShaaban <89933999+AbdullahShaaban@users.noreply.github.com>
2022-09-10 10:34:35 +0200

    implement chip option

---
+++ src/app/modules/chip/components/chip-option/chip-option.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChipOptionComponent } from './chip-option.component';
+
+describe('ChipOptionComponent', () => {
+  let component: ChipOptionComponent;
+  let fixture: ComponentFixture<ChipOptionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChipOptionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChipOptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
